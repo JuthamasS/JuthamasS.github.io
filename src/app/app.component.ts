5c9ea13334588e33057fc043c2afa50c0519f1ad
Juthamas Saikaew <pyjamassamajyp@gmail.com>
2023-09-18 00:12:48 +0700

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,16 @@ export class AppComponent {
   i:number = 0;
   products = ['nail','hammer','saw','sling'];
   parentCount = 0;
+  product!: Product;
+  str!: string;
 
+  ngOnInit(){
+    this.product = new Product();
+  }
+  onSubmit(){
+    this.str ="ค่าจากการค้นหา : " + JSON.stringify(this.product);
+    window.alert(this.str);
+  }
   showShopName(){
     return this.ShopName;
   }
@@ -23,5 +32,11 @@ export class AppComponent {
   SearchProduct(){
     this.parentCount = this.parentCount + 1;
   }
+ }
 
+ class Product {
+  productName:string;
+  constructor(parameters="123") {
+    this.productName = parameters;
+  }
 }
